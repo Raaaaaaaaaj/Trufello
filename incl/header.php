ef0c7a059e87f08afbf411a6ec0fa805c56587e6
Raaaaaaaaaj <raj2222ghosh@gmail.com>
2024-09-29 10:42:36 +0530

    Done the file folder rearrangements

---
+++ incl/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>LawnCare - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="main.css">
+  </head>
---
+++ incl/nav.php
@@ -0,0 +1,36 @@
+<body>
+	<nav class="container navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light fixed-top mt-0 mt-lg-3 mt-md-3" id="navbar">
+	    <div class="container-fluid">
+	    
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav m-auto">
+	        	<li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+	        	<li class="nav-item"><a href="about.php" class="nav-link">About us</a></li>
+				<li class="nav-item dropdown">
+					<a href="services.php" class="nav-link">
+					  Services <i class="fas fa-chevron-down ml-1"></i> <!-- Down arrow icon -->
+					</a>
+					<ul class="dropdown-menu">
+					  <li><a href="service1.php" class="dropdown-item">Football Turf</a></li>
+					  <li><a href="service2.php" class="dropdown-item">Swimmng Pool</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Cricket Truf</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Tennis</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Volleyball</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Multi Sport Turf</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Badminton Court</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Landscape Grass</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Project Consultancy</a></li>
+					  <li><a href="service3.php" class="dropdown-item">Basketball Court</a></li>
+					</ul>
+				  </li>
+				<!-- As we dont have any portfolio page we will use the blog page as portfolio page -->
+				<li class="nav-item"><a href="project.php" class="nav-link">Projects</a></li>
+	        	<li class="nav-item"><a href="gallery.php" class="nav-link">Gallery</a></li>
+	          	<li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
