64cc97ff9a03530580529ae7ca9428a09548c093
Raaaaaaaaaj <raj2222ghosh@gmail.com>
2024-10-03 20:42:28 +0530

    Done layout changes

---
+++ incl/recent_projects.php
@@ -0,0 +1,43 @@
+<?php include 'config.php'; ?>
+<section class="testimony-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-7 text-center heading-section ftco-animate">
+                <span class="subheading">Our Recent Projects</span>
+                <h2 class="mb-4">Here are some of or latest projects</h2>
+            </div>
+        </div>
+        <div class="row d-flex">
+          <div class="col-md-4 d-flex ftco-animate">
+          	<div class="blog-entry justify-content-end">
+              <a href="blog-single.html" class="block-20" style="background-image: url('<?php echo BASE_URL; ?>/images/image_4.jpg');">
+              </a>
+              <div class="text p-4 float-right d-block">
+              	<h3 class="heading mb-0"><a href="#">All you want to know about industrial laws</a></h3>                
+                <p><a href="#" class="btn btn-primary d-none">Read more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex ftco-animate">
+          	<div class="blog-entry justify-content-end">
+              <a href="blog-single.html" class="block-20" style="background-image: url('<?php echo BASE_URL; ?>/images/image_5.jpg');">
+              </a>
+              <div class="text p-4 float-right d-block">              	
+              	<h3 class="heading mb-0"><a href="#">All you want to know about industrial laws</a></h3>
+                <p><a href="#" class="btn btn-primary d-none">Read more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex ftco-animate">
+          	<div class="blog-entry justify-content-end">
+              <a href="blog-single.html" class="block-20" style="background-image: url('<?php echo BASE_URL; ?>/images/image_6.jpg');">
+              </a>
+              <div class="text p-4 float-right d-block">              
+              	<h3 class="heading mb-0"><a href="#">All you want to know about industrial laws</a></h3>                
+                <p><a href="#" class="btn btn-primary d-none">Read more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</section>
--- service_pages/football_turf.php
+++ service_pages/football_turf.php
@@ -1,5 +1,17 @@
 <?php include '../incl/config.php' ?>
 <?php include '../incl/header.php' ?>
 <?php include '../incl/nav.php' ?>
+<section class="hero-wrap hero-wrap-2" style="background-image: url('<?php echo BASE_URL; ?>/images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-end justify-content-center">
+            <div class="col-md-9 ftco-animate pb-5 text-center">
+                <h1 class="mb-3 bread">Football Turf</h1>
+                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include '../incl/recent_projects.php' ?>
 <?php include '../incl/cta.php' ?>
 <?php include '../incl/footer.php' ?>
