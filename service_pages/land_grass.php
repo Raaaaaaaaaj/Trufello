ca580f9098472d07b9f91cbc0b29449ab5b59a21
Raaaaaaaaaj <raj2222ghosh@gmail.com>
2024-10-03 23:08:21 +0530

    Folder updated

--- service_pages/land_grass.php
+++ service_pages/land_grass.php
@@ -12,6 +12,83 @@
         </div>
     </div>
 </section>
+<section class="ftco-section ftco-no-pt">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3 py-5 order-md-last">
+				<div class="heading-section ftco-animate">
+					<span class="subheading">Our way</span>
+					<h2 class="mb-4">Landscape Grass</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi illo fuga veritatis? Nesciunt, rem dolore ratione tenetur tempora iste. Nam esse sint fuga distinctio suscipit?</p>
+					<p><a href="#" class="btn btn-primary py-3 px-4">Get a Quote</a></p>
+				</div>
+			</div>
+			<div class="col-lg-9 services-wrap px-4 pt-5">
+				<div class="row pt-md-3">
+					<div class="col-md-4 d-flex align-items-stretch">
+						<div class="services text-center">
+							<div class="icon d-flex justify-content-center align-items-center">
+								<span class="flaticon-fence"></span>
+							</div>
+							<div class="text">
+								<h3>Garden Care</h3>
+								<p>Seeking justice in the world is a sed significant emotional and investment when we follow this call.</p>
+							</div>
+							<a href="#" class="btn-custom d-flex align-items-center justify-content-center"><span class="ion-ios-arrow-round-forward"></span></a>
+						</div>
+					</div>
+					<div class="col-md-4 d-flex align-items-stretch">
+						<div class="services text-center">
+							<div class="icon d-flex justify-content-center align-items-center">
+								<span class="flaticon-lawn-mower"></span>
+							</div>
+							<div class="text">
+								<h3>Lawn mowing</h3>
+								<p>Seeking justice in the world is a sed significant emotional and investment when we follow this call.</p>
+							</div>
+							<a href="#" class="btn-custom d-flex align-items-center justify-content-center"><span class="ion-ios-arrow-round-forward"></span></a>
+						</div>
+					</div>
+					<div class="col-md-4 d-flex align-items-stretch">
+						<div class="services text-center">
+							<div class="icon d-flex justify-content-center align-items-center">
+								<span class="flaticon-natural-resources"></span>
+							</div>
+							<div class="text">
+								<h3>Lawn care</h3>
+								<p>Seeking justice in the world is a sed significant emotional and investment when we follow this call.</p>
+							</div>
+							<a href="#" class="btn-custom d-flex align-items-center justify-content-center"><span class="ion-ios-arrow-round-forward"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="ftco-section ftco-no-pt ftco-no-pb bg-light">
+    	<div class="container">
+    		<div class="row d-flex py-5">
+    			<div class="col-md-6 d-flex">
+    				<div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-end 70vh" style="background-image:url(<?php echo BASE_URL; ?>/images/about.jpg);">
+    					
+    				</div>
+    			</div>
+    			<div class="col-md-6 pl-md-5">
+    				<div class="row justify-content-start py-5">
+		          <div class="col-md-12 heading-section ftco-animate">
+		          	<span class="subheading">About Our Company</span>
+		            <h2 class="mb-4">Welcome to Turfello</h2>
+		            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+		            <div class="about-text">
+		            	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia perferendis dignissimos eaque accusantium, ad fuga nemo ducimus reprehenderit voluptatum enim ut quia aliquid illo officia aliquam amet temporibus. Saepe esse accusantium architecto, doloribus soluta doloremque assumenda. Dignissimos hic corporis eius laudantium provident, aperiam, cupiditate, odio quibusdam quo culpa commodi. Eaque quisquam fugiat harum eligendi fuga. Ipsum sapiente mollitia incidunt excepturi.
+		            </div>
+		          </div>
+		        </div>
+	        </div>
+        </div>
+    	</div>
+</section>
 <?php include '../incl/recent_projects.php' ?>
 <?php include '../incl/cta.php' ?>
 <?php include '../incl/footer.php' ?>
